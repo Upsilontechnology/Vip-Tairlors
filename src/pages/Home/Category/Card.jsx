21fcf54497b4e1ad6079e6a4e690dff87e08df45
mdshahed89 <lifetime8954@gmail.com>
2024-01-29 22:10:35 +0600

    faq added

--- src/pages/Home/Category/Card.jsx
+++ src/pages/Home/Category/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import CategoryImg from "../../../assets/CategoryImg.png"
+import Button from './Button'
 
 const Card = () => {
   return (
-    <div className='px-3 py-2 border-2 cursor-pointer max-w-[7rem] flex flex-col justify-center items-center hover:border-teal-200 hover:duration-500'>
-      <div className='w-[5rem] bg-slate-100'>
+    <div className='px-3 py-2 border-2 cursor-pointer min-w-[10rem] flex flex-col rounded shadow-lg justify-center items-center hover:border-teal-200 hover:duration-500'>
+      <div className='w-[8rem] bg-slate-100 rounded'>
         <img src={CategoryImg} alt="categoryImg" className='w-full h-auto object-cover'/>
       </div>
-      <div className='flex justify-center mt-3'>
+      <div className='flex justify-center flex-col items-center mt-2'>
         <h3>পাঞ্জাবি</h3>
+        <Button />
       </div>
     </div>
   )
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import Faq from '../Faq/Faq';
 const Home = () => {
     return (
         <div>
             <Banner />
             <Category />
+            <Faq />
         </div>
     );
 };
