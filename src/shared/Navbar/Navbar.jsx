cd9c1f7920b78ef3761465d546350d142857c88a
mdshahed89 <lifetime8954@gmail.com>
2024-02-04 21:21:21 +0600

    register page addedd

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { IoPersonOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 
 const Navbar = () => {
     return (
-        <div className='max-w-[1280px] w-full mx-auto py-2'>
+        <div className='bg-[#EFF4F7]'>
+            <div className='max-w-[1280px] w-full mx-auto py-2 '>
             <div className="navbar">
                 <div className="navbar-start">
                     <a className="btn btn-ghost text-xl">HakkuTraders</a>
                 </div>
-                <div className="w-44 md:w-[600px] lg:w-[700px]">
+                <div className="w-44 md:w-[600px] lg:w-[700px] relative">
                     <div className="form-control w-full">
                         <input type="text" placeholder="Search your product" className="input input-bordered" />
+                    <IoIosSearch className='text-2xl absolute right-0 mr-2 top-[25%] text-gray-500' />
                     </div>
                 </div>
                 <div className="navbar-end">
                     <button className="btn"><IoPersonOutline className='text-xl'/> Logout</button>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
