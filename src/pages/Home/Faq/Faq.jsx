64786e9495b10522d5bbed4f0da8cd90db66186f
studentAbuSayeed <studentabusayeed2@gmail.com>
2024-02-06 03:42:50 +0600

    faq added

--- src/pages/Home/Faq/Faq.jsx
+++ src/pages/Home/Faq/Faq.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
-import FaqImg from "../../../assets/faqImg.png"
+import React from 'react';
+import faqImg from '../../../assets/faq1.png';
+
 
 const Faq = () => {
   return (
-    <div className='supershop-container'>
-      <div className='bg-[#0A1D56] text-white p-5 rounded py-10 lg:flex md:flex justify-between items-center'>
-        <div className='pl-5'>
-            <h3 className='text-2xl lg:text-4xl md:text-3xl font-semibold text-[#f5f5f5]'>Optimizing Stocks <br /> for Maximum Efficiency</h3>
-            <p className='mt-10 text-base lg:text-xl max-w-[80%] text-[#dbd9d9]'>Explore strategic stock allocation, streamline inventory processes, and implement data-driven decisions to enhance operational efficiency and maximize profitability in stock management for sustainable business growth.</p>
-        </div>
-        <div className='lg:min-w-[35rem] md:min-w-[25rem] min-w-[10rem]'>
-            <img src={FaqImg} alt="" className='w-full h-auto object-cover' />
+    <section className='w-full'>
+      <div className="supershop-container rounded-t-md relative bg-[#0a1d56] ring-1 ring-gray-900/5  text-white">
+        <div className="">
+          <div className='md:flex justify-between gap-20 mt-5'>
+            <div className='mx-auto md:w-[60%]'>
+              <div className="collapse collapse-plus flexcode-banner-bg rounded-lg shadow-2xl border-teal-600 border-[2px] mb-4">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium" >
+                  <span className='mr-4 text-lg font-normal'>01</span>What is stock management in a supermarket?
+                </div>
+                <div className="collapse-content">
+                  <p className='text-slate-300 ml-9 text-md font-normal mr-4'>Stock management in a supermarket involves keeping track of inventory, ensuring shelves are stocked with products, and replenishing items as needed to meet customer demand.</p>
+                </div>
+              </div>
+              <div className="collapse collapse-plus flexcode-banner-bg rounded-lg shadow-2xl border-teal-600 border-[2px] mb-4">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                  <span className='mr-4 text-lg font-normal'>02</span>Why is stock management important for a supermarket?
+                </div>
+                <div className="collapse-content">
+                  <p className='text-slate-300 ml-9 text-md font-normal mr-4'>Stock management ensures that customers find the products they need when they visit the supermarket, helps reduce instances of out-of-stock items, and optimizes inventory levels to prevent overstocking or understocking.</p>
+                </div>
+              </div>
+              <div className="collapse collapse-plus flexcode-banner-bg rounded-lg shadow-2xl border-teal-600 border-[2px] mb-4">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                  <span className='mr-4 text-lg font-normal'>03</span>What happens if there's too much stock in the supermarket?
+                </div>
+                <div className="collapse-content">
+                  <p className='text-slate-300 ml-9 text-md font-normal mr-4'>Excess stock can tie up capital, lead to product spoilage or obsolescence, and increase storage costs for the supermarket.</p>
+                </div>
+              </div>
+              <div className="collapse collapse-plus flexcode-banner-bg rounded-lg shadow-2xl border-teal-600 border-[2px] mb-4">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                  <span className='mr-4 text-lg font-normal'>04</span>What happens if there's not enough stock in the supermarket?
+                </div>
+                <div className="collapse-content">
+                  <p className='text-slate-300 ml-9 text-md font-normal mr-4'>Insufficient stock can result in lost sales, disappointed customers, and harm to the reputation of the supermarket.</p>
+                </div>
+              </div>
+              <div className="collapse collapse-plus flexcode-banner-bg rounded-lg shadow-2xl border-teal-600 border-[2px] mb-4">
+                <input type="radio" name="my-accordion-3" />
+                <div className="collapse-title text-xl font-medium">
+                  <span className='mr-4 text-lg font-normal'>05</span>How can I keep track of inventory levels in my supermarket?
+                </div>
+                <div className="collapse-content">
+                  <p className='text-slate-300 ml-9 text-md font-normal mr-4'>You can use inventory management software or manual methods like spreadsheet tracking to monitor inventory levels and ensure timely restocking of products.</p>
+                </div>
+              </div>
+            </div>
+            <div className='w-full md:w-[40%] pt-5'>
+              <img className='rounded-lg w-[380px]' src={faqImg} alt="" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Faq
+export default Faq;
