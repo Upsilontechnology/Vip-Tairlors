eabf9ffd80ce422c96e672aa8a1a2abb22f8bd9f
mdshahed89 <lifetime8954@gmail.com>
2024-02-07 00:35:40 +0600

    quote added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -5,10 +5,6 @@ import React from 'react';
 const Banner = () => {
 
     return (
-<<<<<<< HEAD
-        <div>
-            hello world
-=======
         <div className='flex gap-5 my-10 h-[100vh] text-white supershop-container'>
             <div className='w-1/2 bg-blue-600 h-5/6 rounded-r-full rounded-tl-full'>
                 <div className='flex flex-col ml-5 space-y-3 items-start justify-center h-full'>
@@ -19,7 +15,6 @@ const Banner = () => {
             <div className='w-1/2'>
                 <img className='' src="https://i.ibb.co/YBwJvbg/stock-banner.png" alt="" />
             </div>
->>>>>>> 29c40ff74b3472cfd68478fa3d8d850baa6f7656
         </div>
     );
 };
