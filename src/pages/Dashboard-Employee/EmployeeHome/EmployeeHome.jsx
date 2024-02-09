db811e7bbaa6c5a1671d03a7bc09c5ef3516d65c
mdshahed89 <lifetime8954@gmail.com>
2024-02-09 23:59:02 +0600

    employee home updated

--- src/pages/Dashboard-Employee/EmployeeHome/EmployeeHome.jsx
+++ src/pages/Dashboard-Employee/EmployeeHome/EmployeeHome.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { IoBagOutline } from "react-icons/io5";
+import { FaCalculator } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
 
 const EmployeeHome = () => {
     return (
         <div>
-            <h1>THis is employee home</h1>
+            <h1 className='text-center block mt-[2rem] text-5xl  font-semibold  '>Welcome</h1>
+            <div className='flex justify-center mt-10'>
+
+                <div className='flex gap-10'>
+
+                    <div className='max-w-[25rem] border pr-[3rem] border-gray-400 shadow-md rounded-md flex gap-5 items-center p-4 '>
+                        <div className='p-3 bg-[#9da6c0] rounded-lg '>
+                            <div className='bg-[#0a1d56] p-2 rounded-lg text-white text-xl '>
+                                <IoBagOutline className='font-semibold' />
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className='text-[1.15rem] font-medium '>Total Sales</h3>
+                            <h2 className='text-[1.3rem] font-semibold '>15000 BDT</h2>
+                        </div>
+                    </div>
+
+                    <div className='max-w-[25rem] pr-[3rem] border border-gray-400 shadow-md rounded-md flex gap-5 items-center p-4 '>
+                        <div className='p-3 bg-[#9da6c0] rounded-lg '>
+                            <div className='bg-[#0a1d56] p-2 rounded-lg text-white text-xl '>
+                                <FaCalculator className='font-semibold' />
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className='text-[1.15rem] font-medium '>Total Expenses</h3>
+                            <h2 className='text-[1.3rem] font-semibold '>15000 BDT</h2>
+                        </div>
+                    </div>
+
+                    <div className='max-w-[25rem] pr-[3rem] border border-gray-400 shadow-md rounded-md flex gap-5 items-center p-4 '>
+                        <div className='p-3 bg-[#9da6c0] rounded-lg '>
+                            <div className='bg-[#0a1d56] p-2 rounded-lg text-white text-xl '>
+                                <BsCart3 className='font-semibold' />
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className='text-[1.15rem] font-medium '>Total Orders</h3>
+                            <h2 className='text-[1.3rem] font-semibold '>10</h2>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/pages/Home/QuoteBanner/QuoteBanner.jsx
+++ src/pages/Home/QuoteBanner/QuoteBanner.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import QuoteImg from "../../../assets/quoteBanner.png"
 import AdminImg from "../../../assets/admin.png"
+import SectionTitle from '../../../components/SectionTitle/SectionTitle'
 
 const QuoteBanner = () => {
   return (
     <div className='supershop-container '>
+      <div>
+      <SectionTitle
+                title="Owner Insights"
+                descrition="Gain valuable insights into your business with our Owner Insights section."
+            />
+      </div>
       <div className='flex xl:flex-row flex-col gap-20 xl:gap-0 justify-center xl:justify-between p-[2rem]  shadow-lg rounded px-[0rem] lg:px-[4rem]  bg-[#EFF4F7] '>
         <div className=' flex flex-col justify-center items-center '>
           <div className='  w-[20rem] pt-5 pl-3  rounded-lg bg-slate-400'>
