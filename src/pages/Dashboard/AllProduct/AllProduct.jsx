6b3015d831e3631cd7aa8f65db2d16fc6a093023
MoRsHeD <mdmorshed0187@gmail.com>
2024-03-07 00:20:49 +0600

    updated dashboard,product details and others

--- src/pages/Dashboard/AllProduct/AllProduct.jsx
+++ src/pages/Dashboard/AllProduct/AllProduct.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import ProductDetails from '../../../components/ProductDetails/ProductDetails';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import useSellProduct from '../../../hooks/useSellProduct';
+import useUser from '../../../hooks/useUser';
+import useAuth from '../../../hooks/useAuth';
 
 const AllProduct = () => {
     const [sellProducts, refetch] = useSellProduct();
-    
+    const [filteredUser, setFilterdUser] = useState();
+    const [users] = useUser();
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const findUser = users?.filter(person => person?.email === user?.email);
+        setFilterdUser(findUser)
+    }, [])
+
+    const filteredSells = sellProducts?.filter(product => product?.email === user?.email);
 
     return (
         <div className='supershop-container'>
@@ -20,7 +31,12 @@ const AllProduct = () => {
             </div>
             {/* all product */}
             <div className=''>
-                <ProductDetails />
+                {
+                    filteredUser?.map(user => user?.role === 'employee' ?
+                        <ProductDetails filteredSells={filteredSells} /> :
+                        <ProductDetails filteredSells={sellProducts} />)
+                }
+                {/* <ProductDetails /> */}
             </div>
         </div>
     );
