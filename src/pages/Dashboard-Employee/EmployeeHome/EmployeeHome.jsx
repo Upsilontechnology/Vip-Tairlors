3cc5a124210e93b945533f9bc25de699ec1189f9
MoRsHeD <mdmorshed0187@gmail.com>
2024-03-31 23:09:09 +0600

    updated employeehome

--- src/pages/Dashboard-Employee/EmployeeHome/EmployeeHome.jsx
+++ src/pages/Dashboard-Employee/EmployeeHome/EmployeeHome.jsx
@@ -3,17 +3,19 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import timg from "../../../assets/Group94.png";
 const EmployeeHome = () => {
   return (
-    <div className="mt-4 lg:h-screen h-auto bg-white mx-4 lg:mx-0 lg:ml-10 rounded-md">
-      <div className="flex flex-col-reverse lg:flex-col">
-        <div className="flex justify-center items-center p-10">
-          <SectionTitle
-            title="Welcome to professional dashboard"
-            descrition="Insights, management tools and ad creation - all in one place"
-          />
-        </div>
-        {/* card container */}
-        <div className="flex justify-center items-center">
-          <img src={timg} className="w-2/4 pt-2 pl-7" alt="" />
+    <div className="">
+      <div className="lg:h-[86vh] h-auto bg-white mx-4 lg:mx-0 lg:ml-10 rounded-md">
+        <div className="flex flex-col-reverse lg:flex-col">
+          <div className="flex justify-center items-center p-10">
+            <SectionTitle
+              title="Welcome to professional dashboard"
+              descrition="Insights, management tools and ad creation - all in one place"
+            />
+          </div>
+          {/* card container */}
+          <div className="flex justify-center items-center">
+            <img src={timg} className="w-2/4 pt-2 pl-7" alt="" />
+          </div>
         </div>
       </div>
     </div>
