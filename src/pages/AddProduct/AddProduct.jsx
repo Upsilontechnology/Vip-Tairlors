7f2f947930db26984c477ae3fb4c0fc6704e1a7d
TahidulAlam <tahid.cse@gmail.com>
2024-04-04 02:59:51 +0600

    update

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -5,6 +5,7 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import DashBoardTitle from "../../components/dashboardTitle/DashBoardTitle";
+import { useQuery } from "@tanstack/react-query";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSITNG_KEY;
 const image_hosing_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -17,7 +18,13 @@ const AddProduct = () => {
   } = useForm();
   const axiosPublic = useAxiosPublic();
   const { user } = useAuth();
-
+  const { data: categories = [] } = useQuery({
+    queryKey: ["categoryData"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/category");
+      return res.data;
+    },
+  });
   const onSubmit = (data) => {
     // sending image to the imageBB
     const imageFile = { image: data.image[0] };
@@ -98,7 +105,7 @@ const AddProduct = () => {
                     {...register("name", { required: true })}
                     type="text"
                     placeholder="Product Name"
-                    className="input input-bordered w-full focus:outline-none"
+                    className="input input-bordered w-full focus:outline-none bg-[#F0F2F5]"
                   />
                   {errors.name && (
                     <span className="text-red-500">
@@ -115,7 +122,7 @@ const AddProduct = () => {
                     {...register("quantity", { required: true })}
                     type="number"
                     placeholder="Quantity"
-                    className="input input-bordered w-full focus:outline-none"
+                    className="input input-bordered w-full focus:outline-none bg-[#F0F2F5]"
                   />
                   {errors.quantity && (
                     <span className="text-red-500">Quantity is required</span>
@@ -132,7 +139,7 @@ const AddProduct = () => {
                     {...register("price", { required: true })}
                     type="number"
                     placeholder="Price"
-                    className="input input-bordered focus:outline-none w-full"
+                    className="input input-bordered focus:outline-none w-full bg-[#F0F2F5]"
                   />
                   {errors.price && (
                     <span className="text-red-500">Price is required</span>
@@ -147,7 +154,7 @@ const AddProduct = () => {
                     {...register("date", { required: true })}
                     type="date"
                     placeholder="Date"
-                    className="input input-bordered focus:outline-none w-full"
+                    className="input input-bordered focus:outline-none w-full bg-[#F0F2F5]"
                   />
                   {errors.date && (
                     <span className="text-red-500">Date is required</span>
@@ -160,7 +167,7 @@ const AddProduct = () => {
                   {/* <label className="label">
                     <span className="label-text">Category*</span>
                   </label> */}
-                  <select
+                  {/* <select
                     defaultValue="default"
                     {...register("category", { required: true })}
                     className="select select-bordered focus:outline-none w-full"
@@ -176,7 +183,19 @@ const AddProduct = () => {
                   </select>
                   {errors.category && (
                     <span className="text-red-500">Category is required</span>
-                  )}
+                  )} */}
+                  <select
+                    className="select select-bordered w-full focus:outline-none bg-[#F0F2F5]"
+                    // onChange={(e) => handleCategory(e.target.value)}
+                    {...register("category", { required: true })}
+                    // value={filter}
+                  >
+                    {categories?.map((category, index) => (
+                      <option value={category?.category} key={category._id}>
+                        {category?.category}
+                      </option>
+                    ))}
+                  </select>
                 </div>
                 {/* product Code */}
                 <div className="form-control w-full my-1">
@@ -187,7 +206,7 @@ const AddProduct = () => {
                     {...register("code", { required: true })}
                     type="number"
                     placeholder="Product Code"
-                    className="input input-bordered focus:outline-none w-full"
+                    className="input input-bordered focus:outline-none w-full bg-[#F0F2F5]"
                   />
                   {errors.code && (
                     <span className="text-red-500">
@@ -218,7 +237,7 @@ const AddProduct = () => {
                       {...register("image", { required: false })}
                       type="file"
                       // placeholder="Select Image"
-                      className="text-sm cursor-pointer w-20 hidden"
+                      className="text-sm cursor-pointer w-52 hidden"
                     />
                     <div className=" cursor-pointer px-3">
                       <svg
